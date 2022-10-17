31c741eedbd922ab3342798845f0f33697660ae4
dowoon <woodw920@gmail.com>
2022-10-17 11:18:59 +0900

    #21 - 게시판 뷰 기본 골격 작성
    컨트롤러 핸들러 메소드로 테스트를 통과하게 열어줌
    뷰 적절히 뼈대 잡음
    테스트에서는 media type 검사에서 
    부가적인 옵션이 들어갈 수 있음을 확인했기 때문에
    적절히 테스트 메소드 수정

--- src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -1,5 +1,6 @@
 package com.fastcampus.projectboard.controller;
 
+import com.fastcampus.projectboard.controller.ArticleController;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,62 +22,54 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("개발중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
-    public void given_whenRequestingArticlesView_thenReturnArticlesView() throws Exception {
-        // given
+    public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
+        // Given
 
-        // when & then
+        // When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
-
     }
 
+    @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Disabled("개발중")
-    @DisplayName("[view][GET] 게시글 상세  페이지 - 정상 호출")
     @Test
-    public void given_whenRequestingArticleView_thenReturnArticleView() throws Exception {
-        // given
+    public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
+        // Given
 
-        // when & then
+        // When & Then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(view().name("articles/detail"))
-                .andExpect(model().attributeExists("articles"))
-                .andExpect(model().attributeExists("articlesComments"));
-
+                .andExpect(model().attributeExists("article"));
     }
 
-    @Disabled("개발중")
     @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
+    @Disabled("개발중")
     @Test
-    public void given_whenRequestingArticleSearchView_thenReturnArticleSearchView() throws Exception {
-        // given
+    public void givenNothing_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
+        // Given
 
-        // when & then
+        // When & Then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(view().name("articles/search"));
-
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
     }
 
+    @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출")
     @Disabled("개발중")
-    @DisplayName("[view][GET] 게시글 해시태그 검색  페이지 - 정상 호출")
     @Test
-    public void given_whenRequestingArticleHashtagSearchView_thenReturnArticleHashtagSearchView() throws Exception {
-        // given
+    public void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
+        // Given
 
-        // when & then
+        // When & Then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(view().name("articles/serch-hashtag"));
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
     }
 
 }

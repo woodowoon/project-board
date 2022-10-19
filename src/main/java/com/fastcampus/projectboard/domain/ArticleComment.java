2923bbc95da313c508c0a203373d67a3264a6838
dowoon <woodw920@gmail.com>
2022-10-19 11:30:44 +0900

    #30 - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성
    강의에서 보여주지 않은 나머지 로직의 테스트 코드를 작성하고
    연관된 DTO 를 재설계함
    테스트가 필요로 하는 repository 쿼리 메소드도 작성

--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -3,12 +3,11 @@ package com.fastcampus.projectboard.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
 import javax.persistence.*;
 import java.util.Objects;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "content"),
         @Index(columnList = "createdAt"),
@@ -21,28 +20,29 @@ public class ArticleComment extends AuditingFields {
     private Long id;
 
     @Setter @ManyToOne(optional = false) private Article article; // 게시글 (ID)
-    @Setter @Column(nullable = false, length = 500) private String content; // 댓글 내용
-    public ArticleComment() {
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount; // 유저 정보 (ID)
 
-    }
+    @Setter @Column(nullable = false, length = 500) private String content; // 본문
 
-    private ArticleComment(Article article, String content) {
+
+    protected ArticleComment() {}
+
+    private ArticleComment(Article article, UserAccount userAccount, String content) {
         this.article = article;
+        this.userAccount = userAccount;
         this.content = content;
     }
 
-    public static ArticleComment of(Article article, String content) {
-        return new ArticleComment(article, content);
+    public static ArticleComment of(Article article, UserAccount userAccount, String content) {
+        return new ArticleComment(article, userAccount, content);
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ArticleComment that = (ArticleComment) o;
-        return id.equals(that.id);
+        if (!(o instanceof ArticleComment that)) return false;
+        return id != null && id.equals(that.id);
     }
-
     @Override
     public int hashCode() {
         return Objects.hash(id);

6503fa085ea538d8e2fa2e98cc9b61a45d9446a3
dowoon <woodw920@gmail.com>
2022-10-21 15:44:58 +0900

    #51 - 검색 버그 수정 완료
    검색이 안되는 버그를 수정하였다.
    return empty 를 계속 해주어서 리스트가 뜨지 않았다.

--- src/main/java/com/fastcampus/projectboard/service/ArticleService.java
+++ src/main/java/com/fastcampus/projectboard/service/ArticleService.java
@@ -19,30 +19,29 @@ import javax.persistence.EntityNotFoundException;
 @Transactional
 @Service
 public class ArticleService {
+
     private final ArticleRepository articleRepository;
 
     @Transactional(readOnly = true)
     public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
-        if(searchKeyword == null || searchKeyword.isBlank()) {
-            return articleRepository.findAll(pageable).map(ArticleDto::from); // from 메소드 연결
+        if (searchKeyword == null || searchKeyword.isBlank()) {
+            return articleRepository.findAll(pageable).map(ArticleDto::from);
         }
 
-        switch (searchType) {
+        return switch (searchType) {
             case TITLE -> articleRepository.findByTitleContaining(searchKeyword, pageable).map(ArticleDto::from);
             case CONTENT -> articleRepository.findByContentContaining(searchKeyword, pageable).map(ArticleDto::from);
             case ID -> articleRepository.findByUserAccount_UserIdContaining(searchKeyword, pageable).map(ArticleDto::from);
             case NICKNAME -> articleRepository.findByUserAccount_NicknameContaining(searchKeyword, pageable).map(ArticleDto::from);
             case HASHTAG -> articleRepository.findByHashtag("#" + searchKeyword, pageable).map(ArticleDto::from);
         };
-
-        return Page.empty();
     }
 
     @Transactional(readOnly = true)
     public ArticleWithCommentsDto getArticle(Long articleId) {
         return articleRepository.findById(articleId)
                 .map(ArticleWithCommentsDto::from)
-                .orElseThrow(() -> new EntityNotFoundException("게시글이 없습니다. - articleId: " + articleId));
+                .orElseThrow(() -> new EntityNotFoundException("게시글이 없습니다 - articleId: " + articleId));
     }
 
     public void saveArticle(ArticleDto dto) {
@@ -50,17 +49,14 @@ public class ArticleService {
     }
 
     public void updateArticle(ArticleDto dto) {
-        try{
+        try {
             Article article = articleRepository.getReferenceById(dto.id());
-
             if (dto.title() != null) { article.setTitle(dto.title()); }
             if (dto.content() != null) { article.setContent(dto.content()); }
             article.setHashtag(dto.hashtag());
         } catch (EntityNotFoundException e) {
-            log.warn("게시글 업데이트 실패. 게시글을 찾을 수 없습니다. - dto : {}", dto);
+            log.warn("게시글 업데이트 실패. 게시글을 찾을 수 없습니다 - dto: {}", dto);
         }
-
-
     }
 
     public void deleteArticle(long articleId) {
@@ -70,4 +66,5 @@ public class ArticleService {
     public long getArticleCount() {
         return articleRepository.count();
     }
+
 }

d039f7ad8e5f36ae68a7b4ca1e7cc54af92b9764
dowoon <woodw920@gmail.com>
2022-10-21 11:10:26 +0900

    #39 - Search 를 구현실패
    Search를 구현해보고 싶었지만 실패가 난다.
    - uri를 제대로 완성되고 쿼리도 제대로 날아가는 것 같은데 
    화면에 게시글 리스트를 뿌려주지 못한다.

--- src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
+++ src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -7,6 +7,7 @@ import com.fastcampus.projectboard.service.ArticleService;
 import com.fastcampus.projectboard.service.PaginationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
@@ -15,45 +16,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.data.domain.Pageable;
-
 import java.util.List;
-
 @RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
-
     private final ArticleService articleService;
     private final PaginationService paginationService;
-
     @GetMapping
     public String articles(
             @RequestParam(required = false) SearchType searchType,
             @RequestParam(required = false) String searchValue,
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-            ModelMap map) {
-
+            ModelMap map
+    ) {
         Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
-
         List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
 
         map.addAttribute("articles", articles);
         map.addAttribute("paginationBarNumbers", barNumbers);
-        map.addAttribute("totalCount", articleService.getArticleCount());
+        map.addAttribute("searchTypes", SearchType.values());
 
         return "articles/index";
     }
-
     @GetMapping("/{articleId}")
     public String article(@PathVariable Long articleId, ModelMap map) {
-
         ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
-
         map.addAttribute("article", article);
         map.addAttribute("articleComments", article.articleCommentsResponse());
-
+        map.addAttribute("totalCount", articleService.getArticleCount());
         return "articles/detail";
     }
-
 }

bcd92ca52ccd3853321b30634dc898f043e9d34b
dowoon <woodw920@gmail.com>
2022-10-24 16:04:40 +0900

    #58 - 리팩토링: UserAccountDto 에 메타 정보를 넣지 않는 of() 팩토리 메소드 추가
    테스트를 용이하게 하기 위해 추가함
    
    테스트 목적 외에도,
    repository 레이어에 영속되지 않은 객체는
    메타 정보가 없는 것이 당연하므로
    새로운 데이터를 만들 때 이 부분을 입력하지 않아도 되는
    팩토리 메소드를 만들어주는 것이 의미가 있음

--- src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
+++ src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -16,9 +16,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
 @RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
@@ -39,17 +37,14 @@ public class ArticleController {
         map.addAttribute("searchTypes", SearchType.values());
         return "articles/index";
     }
-
     @GetMapping("/{articleId}")
     public String article(@PathVariable Long articleId, ModelMap map) {
         ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticleWithComments(articleId));
-
         map.addAttribute("article", article);
         map.addAttribute("articleComments", article.articleCommentsResponse());
         map.addAttribute("totalCount", articleService.getArticleCount());
         return "articles/detail";
     }
-
     @GetMapping("/search-hashtag")
     public String searchArticleHashtag(
             @RequestParam(required = false) String searchValue,
@@ -63,53 +58,42 @@ public class ArticleController {
         map.addAttribute("hashtags", hashtags);
         map.addAttribute("paginationBarNumbers", barNumbers);
         map.addAttribute("searchType", SearchType.HASHTAG);
-
         return "articles/search-hashtag";
     }
-
     @GetMapping("/form")
     public String articleForm(ModelMap map) {
         map.addAttribute("formStatus", FormStatus.CREATE);
-
         return "articles/form";
     }
-
     @PostMapping ("/form")
     public String postNewArticle(ArticleRequest articleRequest) {
         // TODO: 인증 정보를 넣어줘야 한다.
         articleService.saveArticle(articleRequest.toDto(UserAccountDto.of(
-                "uno", "asdf1234", "uno@mail.com", "Uno", "memo", null, null, null, null
+                "uno", "asdf1234", "uno@mail.com", "Uno", "memo"
         )));
 
         return "redirect:/articles";
     }
-
     @GetMapping("/{articleId}/form")
     public String updateArticleForm(@PathVariable Long articleId, ModelMap map) {
         ArticleResponse article = ArticleResponse.from(articleService.getArticle(articleId));
-
         map.addAttribute("article", article);
         map.addAttribute("formStatus", FormStatus.UPDATE);
-
         return "articles/form";
     }
-
     @PostMapping ("/{articleId}/form")
     public String updateArticle(@PathVariable Long articleId, ArticleRequest articleRequest) {
         // TODO: 인증 정보를 넣어줘야 한다.
         articleService.updateArticle(articleId, articleRequest.toDto(UserAccountDto.of(
-                "uno", "asdf1234", "uno@mail.com", "Uno", "memo", null, null, null, null
+                "uno", "asdf1234", "uno@mail.com", "Uno", "memo"
         )));
 
         return "redirect:/articles/" + articleId;
     }
-
     @PostMapping ("/{articleId}/delete")
     public String deleteArticle(@PathVariable Long articleId) {
         // TODO: 인증 정보를 넣어줘야 한다.
         articleService.deleteArticle(articleId);
-
         return "redirect:/articles";
     }
-
 }
